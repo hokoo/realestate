65870b78993fc9ce31fe862be1a719b3de5a5673
iTRON Local <root@itron.pro>
2021-02-08 16:00:14 +0300

    meta fields render

---
+++ wp-content/themes/astra-child/inc/meta-fields.php
@@ -0,0 +1,31 @@
+<?php
+add_action('astra_entry_bottom', 'real_estate_mf', 10);
+add_filter( 'real_metadata_value_screen', 'real_metadata_value_screen', 10, 2 );
+
+function real_estate_mf(){
+	$data = get_field_objects();
+	$items = [];
+	foreach ( $data as $field ){
+		$items []= ['tag' => 'col', 'content' => [
+			esc_html( $field['label'] ),
+			esc_html( apply_filters( 'real_metadata_value_screen', $field['value'], $field ) ),
+		]];
+	}
+
+	echo '<!-- ESTATE META DATA -->';
+//	echo '<pre>';
+//	var_dump( $data );
+//	echo '</pre>';
+	real()->render( 'estate-meta', [
+		$items
+	] );
+}
+
+function real_metadata_value_screen( $value, $field ){
+	$result = $value;
+	if ( 'number' == $field['type'] ){
+		$result = number_format( ( int ) $result, 0, '', '&nbsp;' );
+	}
+
+	return $result;
+}
---
+++ wp-content/themes/astra-child/templates/html/estate-meta.tpl
@@ -0,0 +1,9 @@
+<div class="estate-metadata">
+    [[col]]
+    <div class="ast-col-xs-6 metadata">
+        <span class="label">%1$s:</span><br/>
+        <span class="value">%2$s</span>
+    </div>
+    [[/col]]
+    %1$s
+</div>
